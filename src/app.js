6bdc392685c01b193b208438aadc88331ccff2cf
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-26 18:12:58 +0000

    creada la lógica en el app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let noun1 = [
+    "El monstruo",
+    "El psicópata",
+    "El loco",
+    "El ente",
+    "El hechicero",
+    "El fantasma",
+    "El vampiro",
+    "El zombi",
+    "El chupacabras",
+    "El asaltador"
+  ];
+
+  let adjective = [
+    "tenebroso",
+    "oscuro",
+    "horroroso",
+    "maligno",
+    "sobrenatural",
+    "deborador",
+    "horripilante",
+    "macabro",
+    "inquietante",
+    "sobrecogedor"
+  ];
+
+  let noun2 = [
+    "del pantano",
+    "del bosque fantasmal",
+    "del cementerio abandonado",
+    "del túnel",
+    "del ascensor",
+    "de la bahía lúgubre",
+    "del averno",
+    "del oscuro pasillo",
+    "del sótano húmedo",
+    "del universo paralelo"
+  ];
+  function randomName() {
+    let randomNoun1 = noun1[Math.floor(Math.random() * noun1.length)];
+    let randomAdjective =
+      adjective[Math.floor(Math.random() * adjective.length)];
+    let randomNoun2 = noun2[Math.floor(Math.random() * noun2.length)];
+
+    return `${randomNoun1} ${randomAdjective} ${randomNoun2}`;
+  }
 };
