44109f23d653816809e2b107c70e8894b04cf2fe
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-27 11:00:12 +0000

    Añadida la lógica para la descricción de los villanos

--- src/app.js
+++ src/app.js
@@ -55,11 +55,61 @@ window.onload = function() {
     "de la dimensión desconocida",
     "del mundo de los sueños"
   ];
+
   function randomVillainName() {
     let randomNoun1 = noun1[Math.floor(Math.random() * noun1.length)];
     let randomAdjective =
       adjective[Math.floor(Math.random() * adjective.length)];
     let randomNoun2 = noun2[Math.floor(Math.random() * noun2.length)];
+    if (randomNoun1 === "El monstruo") {
+      document.getElementById("villainDescription").textContent =
+        "Ser de naturaleza sobrenatural o artificial que causa miedo o terror. Puede ser una criatura grotesca o monstruosa, o simplemente algo que es desconocido o inexplicable. Los monstruos suelen representar lo desconocido o lo que nos aterroriza.";
+    } else if (randomNoun1 === "El psicópata") {
+      document.getElementById("villainDescription").textContent =
+        "Persona con rasgos antisociales, egocéntricos y violentos. Los psicópatas suelen carecer de empatía y remordimiento, y son propensos a la violencia. Los psicópatas pueden ser muy peligrosos, y a menudo son responsables de crímenes violentos.";
+    } else if (randomNoun1 === "El loco") {
+      document.getElementById("villainDescription").textContent =
+        "Persona que ha perdido la razón o el juicio. Los locos suelen tener problemas para comprender la realidad y pueden tener comportamientos o creencias que son considerados extraños o irracionales. Los locos pueden ser peligrosos, pero también pueden ser víctimas de su propia condición.";
+    } else if (randomNoun1 === "El ente") {
+      document.getElementById("villainDescription").textContent =
+        "Ser o entidad sobrenatural o no identificada. Los entes suelen ser seres misteriosos o inexplicables, y pueden ser objeto de creencias o supersticiones. Los entes pueden ser buenos o malos, y pueden tener un impacto significativo en la vida de las personas que los experimentan.";
+    } else if (randomNoun1 === "El hechicero") {
+      document.getElementById("villainDescription").textContent =
+        "Ser que usa la magia para controlar fuerzas sobrenaturales. Los hechiceros suelen ser poderosos y pueden realizar una variedad de tareas, como lanzar hechizos, crear pociones y convocar espíritus.";
+    } else if (randomNoun1 === "El fantasma") {
+      document.getElementById("villainDescription").textContent =
+        "Alma de un difunto que regresa al mundo de los vivos. Los fantasmas suelen estar asociados con la muerte y el miedo, y pueden ser benevolentes o malévolos.";
+    } else if (randomNoun1 === "El vampiro") {
+      document.getElementById("villainDescription").textContent =
+        "Ser sobrenatural que se alimenta de sangre humana. Los vampiros suelen ser inmortales y tienen una variedad de poderes sobrenaturales, como la fuerza, la velocidad y la capacidad de volar.";
+    } else if (randomNoun1 === "El zombi") {
+      document.getElementById("villainDescription").textContent =
+        "Cadáver reanimado que es controlado por un espíritu maligno. Los zombis suelen ser lentos y torpes, pero pueden ser muy peligrosos en grandes números.";
+    } else if (randomNoun1 === "El chupacabras") {
+      document.getElementById("villainDescription").textContent =
+        "Criatura mítica que se cree que ataca y chupa la sangre del ganado, especialmente cabras. Se ha reportado en varios países de América, pero su existencia no ha sido confirmada científicamente.";
+    } else if (randomNoun1 === "El asaltador") {
+      document.getElementById("villainDescription").textContent =
+        "Persona que roba o agrede a alguien de manera violenta. Los asaltantes suelen ser motivados por el dinero, la venganza o el poder. Pueden ser muy peligrosos y pueden causar lesiones graves o incluso la muerte.";
+    } else if (randomNoun1 === "El devorador") {
+      document.getElementById("villainDescription").textContent =
+        "Ser que se alimenta de otros seres vivos. Los devoradores pueden ser animales, plantas o incluso seres sobrenaturales. Pueden ser beneficiosos para el ecosistema, ya que ayudan a controlar las poblaciones de otros seres vivos. Sin embargo, también pueden ser peligrosos para los humanos, ya que pueden transmitir enfermedades o incluso matar.";
+    } else if (randomNoun1 === "El espíritu") {
+      document.getElementById("villainDescription").textContent =
+        "Ser sobrenatural que existe en el mundo espiritual. Los espíritus pueden ser benevolentes o malévolos, y pueden tener una variedad de formas y poderes. Los espíritus suelen estar asociados con la muerte y el más allá, y pueden ser una fuente de inspiración o miedo.";
+    } else if (randomNoun1 === "El espectro") {
+      document.getElementById("villainDescription").textContent =
+        "Entidad sobrenatural que representa la muerte o la destrucción. Los espectros suelen ser representados como seres fantasmagóricos o monstruosos. Pueden ser una fuente de miedo y terror, ya que a menudo son asociados con la muerte y el más allá.";
+    } else if (randomNoun1 === "El intruso") {
+      document.getElementById("villainDescription").textContent =
+        "Persona o cosa que entra en un lugar sin permiso. Los intrusos pueden ser peligrosos, ya que pueden representar una amenaza para la seguridad o la propiedad. Los intrusos también pueden ser una fuente de inquietud o ansiedad, ya que pueden violar la privacidad o la sensación de seguridad de una persona.";
+    } else if (randomNoun1 === "El mutante") {
+      document.getElementById("villainDescription").textContent =
+        "Ser que tiene una mutación genética que le otorga una habilidad o característica sobrenatural. Los mutantes pueden tener una variedad de habilidades, como la fuerza, la velocidad, la telepatía o la capacidad de volar. Los mutantes suelen ser discriminados por los humanos, ya que son considerados diferentes o peligrosos.";
+    } else {
+      document.getElementById("villainDescription").textContent =
+        "Otra descripción.";
+    }
 
     return `${randomNoun1} ${randomAdjective} ${randomNoun2}`;
   }
